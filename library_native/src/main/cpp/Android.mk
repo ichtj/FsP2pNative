9a6419a93cfc9637fa97eac6f15bc3da6e3147b3
ichtj <13652412606@163.com>
2024-03-07 09:23:41 +0800

    添加多架构支持

--- library_native/src/main/cpp/Android.mk
+++ library_native/src/main/cpp/Android.mk
@@ -6,5 +6,10 @@ LOCAL_SRC_FILES += FsPipelineJNI.cpp
 # 添加 C++ 标志和链接库
 LOCAL_CPPFLAGS := -std=c++14
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lm -lz -llog -lstdc++ -L$(LOCAL_PATH)/lib -lfs_p2p
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lm -lz -llog -lstdc++ -lfs_p2p -lmosquitto
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/arm64-v8a
+else
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/armeabi-v7a
+endif
 include $(BUILD_SHARED_LIBRARY)
