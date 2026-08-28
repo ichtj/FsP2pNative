c984bf2618d3efb678a87e961c54526cbd187820
ichtj <31207253+ichtj@users.noreply.github.com>
2026-08-28 10:51:42 +0800

    构建变更

--- library_native/src/main/cpp/Android.mk
+++ library_native/src/main/cpp/Android.mk
@@ -29,7 +29,7 @@ LOCAL_SRC_FILES += BaseXLink.cpp \
                    PutTypeTool.cpp \
                    IBlackCallback.cpp
 # 添加 C++ 标志和链接库
-LOCAL_CPPFLAGS := -std=c++14
+LOCAL_CPPFLAGS := -std=c++17
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lm -lz -llog -lstdc++ -lfs_p2p -lmosquitto
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
